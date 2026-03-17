cf65626d510b55c6cab9d75b7834d964b52c2a6e
Douglas Brian Neves <douglasbrian369@hotmail.com>
2026-03-17 14:52:39 -0300

    Lógica de conexão

---
+++ trojan.py
@@ -0,0 +1,11 @@
+import socket
+
+IP = "192.168.x.x"
+PORT = 443 # Ou 444, porta padrão do netcat
+
+c = socket.socket((socket.AF_NET, socket.SOCK_STREAM))
+C.connect((IP ,PORT))
+
+while True:
+    data = c.recv(1024)
+    print(data)

